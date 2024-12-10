174928235d585fcd78781d306222bdcfb8b8e7d1
anscarterr <ansartal04@gmail.com>
2024-12-10 17:32:41 +0500

    Add files via upload

---
+++ Lab5and6/src/main/java/com/example/Lab5and6/Laboratory5Application.java
@@ -0,0 +1,13 @@
+package com.example.Lab5and6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratory5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratory5Application.class, args);
+	}
+
+}
---
+++ Lab5and6/src/test/java/com/example/Lab5and6/Laboratory5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lab5and6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratory5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
